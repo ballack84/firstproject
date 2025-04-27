d7ddb8551cc587a369c66d2af0c96cd0dcf7d441
ballack84 <haitham.barqawi84@gmail.com>
2025-04-27 20:45:02 +0400

    update

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -1,24 +1,33 @@
+import static org.testng.Assert.assertEquals;
+
 import java.awt.Checkbox;
 import java.awt.RenderingHints.Key;
+import java.awt.Window;
+import java.awt.event.WindowStateListener;
 import java.sql.Driver;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class MyTestCases {
 	
 	WebDriver driver = new ChromeDriver();
 	String website = "https://codenboxautomationlab.com/practice/";
 	Random rand = new Random();
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+
 	
 	@BeforeTest
 	
@@ -37,8 +46,13 @@ public class MyTestCases {
 		//click on first radio button
 		//Allradiobuttons.get(0).click();
 		
+		
+		
 		int randomradiobutton = rand.nextInt(Allradiobuttons.size());
 		Allradiobuttons.get(randomradiobutton).click();
+		//to know if the radio button is really selected
+		boolean excpetedresult = true;
+		boolean actualresult = Allradiobuttons.get(randomradiobutton).isSelected();
 		
 	}
 	
@@ -46,12 +60,38 @@ public class MyTestCases {
 
 	public void DynamicDropdownExample() throws InterruptedException {
 	   
+	   
+	   
+	   
+	   //generate random string in a static way because i dont beed my test to include any other data
 	   String[] countriescode = {"US","OM","BR"};
+	   
+	   //random index based on the lenght of the above array
 	   int randomindex = rand.nextInt(countriescode.length);
+	   
+	   // web element to the input field of country
 	   WebElement randomcountries = driver.findElement(By.id("autocomplete"));
+	   
+	   //send an random item from my array to the webelemrnt (inputfield)
 	   randomcountries.sendKeys(countriescode[randomindex]);
 	   Thread.sleep(1000);
+	   
+	   //it will press arrow down + enter to select first item from list
 	   randomcountries.sendKeys(Keys.chord(Keys.ARROW_DOWN,Keys.ENTER));
+	   
+	   //return first value of the element and i make it string by adding caSTING
+	   //i need to capture or take the country name that selenium already selected
+	  String datainsidemyinput = (String) js.executeScript("return arguments[0].value",randomcountries);
+	  
+	  //the country name for example jordan contains capital letters and small 
+	  //so what i did i make all letters small 
+	  String updatedata = datainsidemyinput.toLowerCase();
+
+      
+	  Boolean actualvalue = updatedata.contains(countriescode[randomindex].toLowerCase());
+
+	   Boolean Excpectedresult = true;
+	  Assert.assertEquals(actualvalue, Excpectedresult);
   }
 	
 	@Test(priority=3,description = "Static Dropdown Example")
@@ -80,6 +120,9 @@ public void checkbpxexample() {
 	//to select them all
 	//for  (int i=0; i<checkboxes.size();i++) {
 	//	checkboxes.get(i).click();
+	//boolean Actualresult = checkboxes.get(i).isselected();
+	// boolean excpetedresult = true;
+	//Assert.assertEquals(Actualresult,excpetedresult);
 		
 	}
 	@Test(priority=5,description = "Switch Window Example",enabled=false)
@@ -152,7 +195,44 @@ public void checkbpxexample() {
 					
 		}
 		}
+	
+	@Test(priority=9,description = "show and hide button")
+	public void elementdisplay() {
+		//hard assert will stop all the execution
+		//soft assert will continoue even if one is failed
 		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scroll, 1500");
+		SoftAssert myassert = new SoftAssert();
+		
+		WebElement hidebutton = driver.findElement(By.id("hide-textbox"));
+		WebElement showbutton = driver.findElement(By.id("show-textbox"));
+		
+		hidebutton.click();
+		 WebElement textinseting = driver.findElement(By.id("displayed-text"));
+		 //soft assert
+		// myassert.assertEquals(textinseting.isDisplayed(), false);
+		 Assert.assertEquals(textinseting.isDisplayed(), false);
+		 
+		 showbutton.click();
+		 Assert.assertEquals(textinseting.isDisplayed(), true);
+		 //for soft assert run
+		// myassert.assertAll();
+		
+	}
+	
+	@Test(priority=10)	
+	
+	public void checkthetitle() {
+		
+		String excpected = "Automation Practice - CodenBox AutomationLab";
+		
+		String actualtitle = driver.getTitle();
+		Assert.assertEquals(actualtitle,excpected);
+	}
+	
+	
+	
 	}
 	
 	
